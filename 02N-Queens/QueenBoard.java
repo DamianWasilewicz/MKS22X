bd93e3a9cfd7336bf48191efe08e95992624cb2f
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-02-10 17:05:57 -0500

    Worked on addqueen

--- 02N-Queens/QueenBoard.java
+++ 02N-Queens/QueenBoard.java
@@ -2,6 +2,9 @@ public class QueenBoard{
     public static void main(String[] args){
 	QueenBoard test = new QueenBoard(5);
 	System.out.println(test.toString());
+  System.out.println("------------------------------");
+  test.addQueen(0, 0);
+  System.out.println(test.toString());
     }
     private int[][] board;
     public QueenBoard(int size){
@@ -14,9 +17,24 @@ public class QueenBoard{
 	board = b;
     }
     private boolean addQueen(int r, int c){
-	if(
-    }
-    
+	     if(board[r][c] == 0){
+         board[r][c] = -1;
+           for(int c1 = 0; c1 < board[r].length; c1++){
+             if(c1!= c){
+               board[r][c1]+=1;
+         }
+         for(int c1 = 0; c1 < board[r].length; c1++){
+           if(c1!= c){
+             board[r][c1]+=1;
+       }
+       }
+         return true;
+       }
+       else{
+         return false;
+       }
+}
+
     public String toString(){
 	String answer = "";
 	int c = 0;
@@ -30,14 +48,16 @@ public class QueenBoard{
 		    answer+= "Q ";
 		    c++;
 		}
+    if(board[counter][counter2] > 0){
+		    answer+= "1 ";
+		    c++;
+		}
 		else{
 		    answer+= "_ ";
 		    c++;
 		}
-	    }
+	  }
 	}
 	return answer;
     }
 }
-	
-        
