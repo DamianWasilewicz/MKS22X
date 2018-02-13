71e293961cc5a1e82b3dcd6059d3f312317da83f
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-02-12 23:46:13 -0500

    Worked on solve

--- 02N-Queens/QueenBoard.java
+++ 02N-Queens/QueenBoard.java
@@ -1,19 +1,21 @@
 public class QueenBoard{
-   
-public class QueenBoard{
     public static void main(String[] args){
-	QueenBoard test = new QueenBoard(5);
-	System.out.println(test.toString());
-	System.out.println("---------------------");
-	test.addQueen(0, 0);
-	System.out.println("---------------------");
-	System.out.println(test.toString());
-	System.out.println("---------------------");
-	test.addQueen(2, 1);
-	System.out.println(test.toString());
-	System.out.println("---------------------");
-	test.removeQueen(0, 0);
-	System.out.println(test.toString());
+	QueenBoard test = new QueenBoard(10);
+	/*System.out.println(test.toString());
+  System.out.println("------------------------------");
+  test.addQueen(0, 0);
+  System.out.println(test.toString());
+  test.addQueen(2,1);
+  System.out.println("------------------------------");
+  System.out.println(test.toString());
+  System.out.println("------------------------------");
+  test.removeQueen(2,1);
+  System.out.println(test.toString());
+  System.out.println("------------------------------");
+  test.removeQueen(0,0);
+  System.out.println(test.toString());
+  System.out.println("------------------------------");*/
+  System.out.println(test.solve());
     }
     private int[][] board;
     public QueenBoard(int size){
@@ -26,35 +28,33 @@ public class QueenBoard{
 	board = b;
     }
     private boolean addQueen(int r, int c){
-	if(board[r][c] != 0){
-	    return false;
-	}
-	for(int c1 = 0; c1< board.length; c1++){
-	    for(int c2 = 0; c2<board[r].length; c2++){
-		if(c1 == r || c2 == c ||
-		   c1 + c2 == r + c || c1 - c2 == r - c){
-		    board[c1][c2]+= 1;
-		}
-	    }
-	}
-	board[r][c] = -1;
-	return true;
-    }
-     private boolean removeQueen(int r, int c){
-	if(board[r][c] != -1){
-	    return false;
-	}
-	for(int c1 = 0; c1< board.length; c1++){
-	    for(int c2 = 0; c2<board[r].length; c2++){
-		if(c1 == r || c2 == c ||
-		   c1 + c2 == r + c || c1 - c2 == r - c){
-		    board[c1][c2]-= 1;
-		}
-	    }
-	}
-	board[r][c] = 0;
-	return true;
-    }
+	     if(board[r][c] != 0){
+         return false;
+       }
+       for(int c1 = 0; c1 < board.length; c1++){
+         for(int c2 = 0; c2 < board[c1].length; c2++){
+           if(c1 == r || c2 == c || Math.abs(c1 - c2) == Math.abs(r-c)){
+             board[c1][c2]+= 1;
+           }
+         }
+       }
+       board[r][c] = -1;
+       return true;
+}
+private boolean removeQueen(int r, int c){
+   if(board[r][c] != -1){
+     return false;
+   }
+   for(int c1 = 0; c1 < board.length; c1++){
+     for(int c2 = 0; c2 < board[c1].length; c2++){
+       if(c1 == r || c2 == c|| Math.abs(c1 - c2) == Math.abs(r-c)){
+         board[c1][c2]-= 1;
+       }
+     }
+   }
+   board[r][c] = 0;
+   return true;
+}
     public String toString(){
 	String answer = "";
 	int c = 0;
@@ -64,23 +64,37 @@ public class QueenBoard{
 		    answer+= "\n";
 		    c = 0;
 		}
-		if(board[counter][counter2] > 0){
-		    answer+= "L ";
-		    c++;
-		}
-		if(board[counter][counter2] < 0){
+		if(board[counter][counter2] == -1){
 		    answer+= "Q ";
 		    c++;
 		}
-		if(board[counter][counter2] == 0){
+    if(board[counter][counter2] > 0){
+		    answer+= "1 ";
+		    c++;
+		}
+    if(board[counter][counter2] == 0){
 		    answer+= "_ ";
 		    c++;
 		}
-	    }
-	}
-	return answer;
+	  }
     }
+    return answer;
 }
-	
-            }
+public boolean solve(){
+  return solveR(0);
+}
+public boolean solveR(int col){
+  if(col >= board.length){
+    return true;
+  }
+  for(int row : board[col]){
+    if(addQueen(row,col)){
+      if(solveR(col + 1)){
+        return true;
+      }
+      removeQueen(row,col);
+    }
+  }
+  return false;
+}
 }
