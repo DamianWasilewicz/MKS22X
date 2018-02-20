9d33d6fbda40dd06dd37e9edf470406603e902b4
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-02-19 23:22:53 -0500

    Worked on solve some more

--- 03KnightsTour/Knightboard.java
+++ 03KnightsTour/Knightboard.java
@@ -1,12 +1,10 @@
-
-
 public class Knightboard{
   public static void main(String[] args){
-	Knightboard test = new Knightboard(10, 10);
+	Knightboard test = new Knightboard(5, 5);
 	System.out.println(test.toString());
   System.out.println(test.solve(0,0));
    }
-   private int[] moveset;
+   private int[][] moveset ={ {2,1} , {2,-1} , {-2, 1} , {-2,1} , {1, 2} , {1, -2} , {-1, 2} , {-1, -2}};
    private int[][] board;
    public Knightboard(int startingRows, int startingCols){
 	if(startingRows < 0 || startingCols < 0){
@@ -14,11 +12,10 @@ public class Knightboard{
 	}
 	int[][] data = new int[startingRows][startingCols];
 	board = data;
-	int[][] moveset  ={ {2,1} , {2,-1} , {-2, 1} , {-2,1} , {1, 2} , {1, -2} , {-1, 2} , {-1, -2}};
    }
 public boolean solve(int StartingRow, int StartingCol){
 	for(int r = 0; r < board.length; r++){
-	    for(int c= 0; c< board[r].length; r++){
+	    for(int c = 0; c< board[r].length; c++){
 		if(board[r][c] != 0){
 		    return false;
 		}
@@ -27,7 +24,7 @@ public boolean solve(int StartingRow, int StartingCol){
 	if(StartingRow < 0 || StartingCol < 0){
 	    return false;
 	}
-	return solveHelper(StartingRow, StartingCol, 0);
+	return solveHelper(StartingRow, StartingCol, 1);
   }
  public boolean solveHelper(int StartingRow, int StartingCol, int level){
    if(level == board.length * board[0].length){
@@ -37,20 +34,29 @@ public boolean solve(int StartingRow, int StartingCol){
 	    if(!(StartingRow + moveset[counter][0] >= board.length||
 	       StartingRow + moveset[counter][0] <= board.length||
 	       StartingCol + moveset[counter][2] >= board[0].length||
-	       StartingCol + moveset[counter][2] <= board[0].length)){
-         return solveHelper(StartingRow + moveset[counter][0], StartingCol + moveset[counter][2], level + 1);
+	       StartingCol + moveset[counter][2] <= board[0].length ||
+         board[StartingRow + moveset[counter][0]][StartingCol + moveset[counter][2]] != 0)) {
+        board[StartingRow + moveset[counter][0]][StartingCol + moveset[counter][2]] += level;
+         if(solveHelper(StartingRow + moveset[counter][0], StartingCol + moveset[counter][2], level + 1)){
+           return true;
+         }
+         else {
+           board[StartingRow + moveset[counter][0]][StartingCol + moveset[counter][2]] = 0;
+         }
          }
        }
        return false;
      }
-
-
 
 public String toString(){
 	int counter = 0;
 	String answer = new String("");
 	for(int r = 0; r<board.length;r++){
 	    for(int c = 0; c<board[r].length;c++){
+    if(board[r][c] != 0){
+    		    answer+= board[r][c];
+    		    counter++;
+    		}
 		if(board[r][c] == 0){
 		    answer+= "__";
 		    counter++;
