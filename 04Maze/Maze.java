369d35eb49c58fa210f0357503a06ee7c9777de5
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-03-04 11:43:20 -0500

    Worked on it

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -1,25 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Maze{
     private char[][]maze;
-    
+
     private boolean animate;
 
     public Maze(String filename) throws FileNotFoundException{
-	char[][] data;
-	mazeInfo = new ArrayList<char[]>();
-	Scanner inf = new Scanner(new File(filename)) ;
-	   while(int.hasNextLine()){
-		String line = inf.next();
-		mazeInfo.add(line.toCharArray());
-	   }
-	   for(int counter = 0; counter < mazeInfo.size(); counter++){
-	       for(int counter1 = 0; counter1 < 
-	        
-	       
+	     char[][] data;
+	     ArrayList mazeInfo = new ArrayList<char[]>();
+	     Scanner inf = new Scanner(new File(filename)) ;
+	     while(inf.hasNextLine()){
+		       String line = inf.next();
+		       mazeInfo.add(line.toCharArray());
+	     }
+	     for(int counter = 0; counter < mazeInfo.size(); counter++){
+	       for(int counter1 = 0; counter1 < mazeInfo.get(0).length; counter1++){
+           data[counter][counter1] = mazeInfo.get(counter)[counter1];
+         }
+       }
+      maze = data;
+      int num = 0;
+      for(int counter = 0; counter < maze.length; counter++){
+        for(int counter1 = 0; counter1 < maze[0].length; counter1++){
+        if(num > 1){
+          throw new IllegalStateException();
+        }
+        if(maze[counter][counter1] == 'E'){
+          num++;
+        }
+        }
     }
+  }
      private void wait(int millis){
          try {
              Thread.sleep(millis);
