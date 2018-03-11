6ec32ee65be369404b92afca624cdefb67502599
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-03-11 17:30:54 -0400

    Got rid of unncesary print lines

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -24,7 +24,7 @@ public class USACO{
         int C2 = in.nextInt() - 1;
        int[][] secondArray = new int[rows][cols];
        secondArray[R1][C1] = 1;
-       //System.out.println(toString(secondArray));
+      
        int timeCounter = 0;
        while(timeCounter < time){
          int[][] thirdArray = new int[rows][cols];
@@ -52,8 +52,7 @@ public class USACO{
            secondArray[r][c] = thirdArray[r][c];
          }
        }
-      // System.out.println(toString(secondArray));
-       //System.out.println(secondArray[R2][C2]);
+
        timeCounter++;
      }
       return secondArray[R2][C2];
@@ -63,23 +62,6 @@ public class USACO{
     }
     return -1;
   }
-  /* char[] firstline = filewords.get(0);
-   int rows = firstline[0];
-   char[] lastline = filewords.get(filewords.size()-1);
-   System.out.println(toString(firstline));
-   System.out.println(toString(lastline));
-   char[][] firstArray = new char[firstline[0]][firstline[2]];
-   System.out.println(rows + " " +4 + " " + firstline[0]);
-   System.out.println(5 + " " + firstline[2]);
-   for(int c1 = 0; c1 < rows; c1++){
-       for(int c2 = 0; c2 < 5; c2++){
-	   firstArray[c1][c2] = filewords.get(c1 + 1)[c2];
-       }
-   }
-   System.out.println(toString(firstArray));
-   int limit = firstline[4];
-   return -1;
-   */
  public static String toString(int[] input){
    String answer = " ";
    for(int c1 = 0; c1 < input.length; c1++){
