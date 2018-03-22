fa6382b0b4cb5da457a8bcaa85d7a9ca7ec6ebf9
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-03-22 00:22:12 -0400

    Finished, tested

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,53 +1,54 @@
 public class Merge{
   public static void main(String[] args){
-      int[] test = {44, 51, 24, 65, 12, 49, 72, 66, 34, 18, 78 , 29, 79, 56 ,34 ,56, 65};
+      int[] test = {1, 300, 20, 50000, 7000000, 4000, 600000, 900000000};
       Merge.mergesort(test);
       System.out.println(Merge.toString(test));
   }
+
   public static void mergesort(int[] data){
-      int[] temp = new int[data.length];
-      msort(data, temp, 0, data.length - 1);
+      int[] temporary = new int[data.length];
+      msort(data, temporary, 0, data.length - 1);
   }
   public static void msort(int[] data, int[] temp, int startP, int endP){
-      if(startP >= endP){
-	  return;
-      }
-      for(int counter = startP; counter <= endP; counter++){
+      if(startP < endP){
+   for(int counter = startP; counter <= endP; counter++){
 	  temp[counter] = data[counter];
-      }
+  }
       int mid = (startP + endP) / 2;
       msort(temp, data, startP, mid);
       msort(temp, data, mid + 1, endP);
-      Merge(data, temp, startP, mid, mid + 1, endP);
+      merge(data, temp, startP, mid, mid + 1, endP);
   }
-	
-	
-    public static void Merge(int[] data1, int[] data2, int sP, int m, int m1, int eP){
-    int[] newData = new int[data1.length + data2.length];
+}
+    public static void merge(int[] data1, int[] data2, int sP, int m, int m1, int eP){
     int tracker1 = sP;
     int tracker2 = m1;
-    while(tracker1 < m && tracker2 < eP){
-      int sum = tracker1 + tracker2;
-      if(data1[tracker1] < data2[tracker2]){
-        newData[sum] = data1[tracker1];
+    for(int counter = tracker1; counter <= eP; counter++){
+      if(tracker1 <= m && tracker2 > eP) {
+          data1[counter] = data2[tracker1];
+          tracker1++;
+        }
+        else if(tracker1 <= m && data2[tracker1] < data2[tracker2]){
+          data1[counter] = data2[tracker1];
+          tracker1++;
+        }
+    /* else if(tracker2 == eP){
+          data1[counter] = data2[tracker1];
+          tracker1++;
+        }
+      else if(data2[tracker1] < data2[tracker2]){
+        data1[counter] = data2[tracker1];
         tracker1++;
-      }
-      else{
-        newData[sum] = data2[tracker2];
+      }*/
+    else {
+    //(data2[tracker1] >= data2[tracker2]){
+        data1[counter] = data2[tracker2];
         tracker2++;
       }
     }
-    if(tracker2 == m){
-      for(int counter = tracker1; counter < data1.length; counter++){
-        newData[counter + tracker2] = data1[counter];
-      }
-    }
-    if(tracker1 == eP){
-      for(int counter = tracker2; counter < data2.length; counter++){
-        newData[counter + tracker1] = data2[counter];
-      }
-    }
-    }
+  }
+
+
   public static String toString(int[] data){
     String answer = new String (" ");
     for(int counter = 0; counter < data.length; counter++){
