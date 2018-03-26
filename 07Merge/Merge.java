8075de380cfef7638a61ef1474e9e843200dfe30
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-03-25 23:03:33 -0400

    Implemented insertionsort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -10,18 +10,21 @@ public class Merge{
       msort(data, temporary, 0, data.length - 1);
   }
   public static void msort(int[] data, int[] temp, int startP, int endP){
-      /* if(startP < endP){
+    /*  if(startP < endP){
    for(int counter = startP; counter <= endP; counter++){
 	  temp[counter] = data[counter];
-	  }*/
-    if(endP - startP < 100){
-	  insertionSort(data, endP, startP);
-	  }else{
+  }*/
+  if(endP - startP <= 10){
+	  insertionSort(data, startP, endP);
+    return;
+  }
+  for(int counter = startP; counter <= endP; counter++){
+    temp[counter] = data[counter];
+  }
       int mid = (startP + endP) / 2;
       msort(temp, data, startP, mid);
       msort(temp, data, mid + 1, endP);
       merge(data, temp, startP, mid, mid + 1, endP);
-      }
   }
     public static void merge(int[] data1, int[] data2, int sP, int m, int m1, int eP){
     int tracker1 = sP;
@@ -49,16 +52,25 @@ public class Merge{
         tracker2++;
       }
     }
-  }
-    public static void insertionSort(int[] data, int sP, int eP){
-    for(int counter = sP; counter <= eP; counter++){
-      for(int counter1 = counter; counter1 <= eP ; counter1++){
-        if(data[counter] < data[counter1]){
-        swap(data, counter1 - 1, counter);
-        }
+  }  public static void insertionSort(int[] data, int sP, int eP){
+    int c;
+      for(int counter = sP; counter <= eP; counter++){
+        int c2 = data[counter];
+        for(c = counter; c > sP && c2 < data[c - 1]; c--){
+          data[c] = data[c - 1];
+          }
+          data[c] = c2;
+         }
+   }
+   /*public static void insertionSort(int[] data, int sP, int eP){
+       for(int counter = sP; counter <= eP; counter++){
+         for(int counter1 = counter; counter1 > sP; counter1--){
+           if(data[counter1] < data[counter1 -1]){
+           swap(data, counter1 - 1, counter1);
+           }
+          }
        }
-    }
-}
+     }*/
  public static void swap(int[] ary, int a, int b){
   int c = ary[a];
   ary[a] = ary[b];
