83bf431669eb365adf0fbe1b19ee2e4bda8b1c78
DamianWasilewicz <dwasilewicz@stuy.edu>
2018-04-18 15:35:44 -0400

    fixed bug need neg

--- 09PostFix/Eval.java
+++ 09PostFix/Eval.java
@@ -1,46 +1,52 @@
 import java.util.*;
 public class Eval{
-  public static void main(String[] args){
-    System.out.println(Eval.eval("5 5 + 10 / 10 + 10 %"));
-  }
-  public static String eval(String expression){
-    LinkedList<String> data = new LinkedList<>();
-    String temp = new String("");
-    for(int c = 0; c < expression.length(); c++){
-      System.out.println(data);
-      if(expression.charAt(c) == '+'){
-        double first = Double.parseDouble(data.pop());
-        double second = Double.parseDouble(data.pop());
-        data.add(second + first + "");
-      }
-      else if(expression.charAt(c) == '-'){
-        double first = Double.parseDouble(data.pop());
-        double second = Double.parseDouble(data.pop());
-        data.add(second - first + "");
-      }
-      else if(expression.charAt(c) == '/'){
-        double first = Double.parseDouble(data.pop());
-        double second = Double.parseDouble(data.pop());
-        data.add(second / first + "");
-      }
-      else if(expression.charAt(c) == '*'){
-        double first = Double.parseDouble(data.pop());
-        double second = Double.parseDouble(data.pop());
-        data.add(second * first + "");
-      }
-      else if(expression.charAt(c) == '%'){
-        double first = Double.parseDouble(data.pop());
-        double second = Double.parseDouble(data.pop());
-        data.add(second % first + "");
-      }
-      else if(expression.charAt(c) != ' '){
-       temp+= expression.charAt(c);
-      }
-      else if(temp.length() > 0){
-        data.add(temp);
-        temp = "";
-      }
+    public static void main(String[] args){
+	System.out.println(Eval.eval("5 4 + 1000 * "));
+	/*System.out.println(Eval.eval("5 4 - 10 * 2 -2 * 3 %"));
+	System.out.println(Eval.eval("5 4 / 10 * 2 - 2 * 3 %"));
+	System.out.println(Eval.eval("5 4 * 10 * 2 - 2 * 3 %"));
+	System.out.println(Eval.eval("5 4 % 10 *2 - 2 * 3 %"));*/
     }
-    return data.getFirst();
-  }
+    public static String eval(String expression){
+	LinkedList<String> data = new LinkedList<>();
+	String temp = "";
+	for(int c = 0; c < expression.length(); c++){
+	    if(expression.charAt(c) == ' '){
+		if(temp.equals('+')){
+		double second = Double.parseDouble(data.pop());
+		double first = Double.parseDouble(data.pop());
+		data.add(second + first + "");
+	    }
+	        if(temp.equals('-')){
+		double second = Double.parseDouble(data.pop());
+		double first = Double.parseDouble(data.pop());
+		data.add(second - first + "");
+	    }
+	        if(temp.equals('/')){
+		double second = Double.parseDouble(data.pop());
+		double first = Double.parseDouble(data.pop());
+		data.add(second / first + "");
+	    }
+	        if(temp.equals ('*')){
+		double second= Double.parseDouble(data.pop());
+		double first  = Double.parseDouble(data.pop());
+		data.add(second * first + "");
+	    }
+		else if(temp.equals('%')){
+		double second= Double.parseDouble(data.pop());
+		double first  = Double.parseDouble(data.pop());
+		data.add(second % first + "");
+		}
+		
+	    }
+	    else{
+		temp+= "" + expression.charAt(c);
+		
+	    else if(temp.length() > 0){
+		data.add(temp);
+		temp = "";
+	    }
+	}
+	return data.getFirst();
+    }
 }
