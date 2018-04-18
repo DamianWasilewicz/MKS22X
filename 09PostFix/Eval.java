c22a95a2e9b40974171ab3f80934c166a12993d4
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-04-18 18:58:29 -0400

    Worked on it :(

--- 09PostFix/Eval.java
+++ 09PostFix/Eval.java
@@ -1,52 +1,48 @@
 import java.util.*;
 public class Eval{
     public static void main(String[] args){
-	System.out.println(Eval.eval("5 4 + 1000 * "));
-	/*System.out.println(Eval.eval("5 4 - 10 * 2 -2 * 3 %"));
-	System.out.println(Eval.eval("5 4 / 10 * 2 - 2 * 3 %"));
-	System.out.println(Eval.eval("5 4 * 10 * 2 - 2 * 3 %"));
-	System.out.println(Eval.eval("5 4 % 10 *2 - 2 * 3 %"));*/
+	System.out.println(Eval.eval("5 4 +"));
     }
     public static String eval(String expression){
 	LinkedList<String> data = new LinkedList<>();
 	String temp = "";
 	for(int c = 0; c < expression.length(); c++){
-	    if(expression.charAt(c) == ' '){
-		if(temp.equals('+')){
-		double second = Double.parseDouble(data.pop());
-		double first = Double.parseDouble(data.pop());
-		data.add(second + first + "");
+	  if(expression.charAt(c) == ' '){
+		    if(temp.equals("+")){
+		        double second = Double.parseDouble(data.pop());
+		          double first = Double.parseDouble(data.pop());
+		            data.add(second + first + "");
+                System.out.println(data);
+	             }
+	      else if(temp.equals("-")){
+		       double second = Double.parseDouble(data.pop());
+		         double first = Double.parseDouble(data.pop());
+		           data.add(second - first + "");
+	            }
+	      else if(temp.equals("/")){
+		       double second = Double.parseDouble(data.pop());
+		         double first = Double.parseDouble(data.pop());
+		           data.add(second / first + "");
+	            }
+	      else if(temp.equals ("*")){
+		        double second= Double.parseDouble(data.pop());
+		          double first  = Double.parseDouble(data.pop());
+		            data.add(second * first + "");
+	             }
+		   else if(temp.equals("%")){
+		       double second= Double.parseDouble(data.pop());
+		         double first  = Double.parseDouble(data.pop());
+		           data.add(second % first + "");
+		           }
+               else{
+                 data.add(temp);
+               }
+               temp = " ";
+	            }
+	  else{
+      temp += expression.charAt(c) + "";
+    }
 	    }
-	        if(temp.equals('-')){
-		double second = Double.parseDouble(data.pop());
-		double first = Double.parseDouble(data.pop());
-		data.add(second - first + "");
-	    }
-	        if(temp.equals('/')){
-		double second = Double.parseDouble(data.pop());
-		double first = Double.parseDouble(data.pop());
-		data.add(second / first + "");
-	    }
-	        if(temp.equals ('*')){
-		double second= Double.parseDouble(data.pop());
-		double first  = Double.parseDouble(data.pop());
-		data.add(second * first + "");
-	    }
-		else if(temp.equals('%')){
-		double second= Double.parseDouble(data.pop());
-		double first  = Double.parseDouble(data.pop());
-		data.add(second % first + "");
-		}
-		
-	    }
-	    else{
-		temp+= "" + expression.charAt(c);
-		
-	    else if(temp.length() > 0){
-		data.add(temp);
-		temp = "";
-	    }
-	}
 	return data.getFirst();
     }
 }
