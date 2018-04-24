c47ecd74ea7914931bcbe5cc0fa0d91a1db809ac
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-04-23 21:44:21 -0400

    Fixed both adds

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -4,142 +4,149 @@ public class MyDeque<E>{
     private int start;
     private int end;
     private int size = 0;
+    public static void main(String[] args){
+      MyDeque<Integer> test = new MyDeque<>();
+      for(int c = 50; c > 0; c--){
+        test.addFirst(c);
+      }
+      /*test.addFirst(10);
+      test.addFirst(9);
+      test.addFirst(8);
+      test.addFirst(7);
+      test.addFirst(6);
+      test.addFirst(5);
+      test.addFirst(4);
+      test.addFirst(3);
+      test.addFirst(2);
+      test.addFirst(1);
+      test.addFirst(0)*/
+      //test.addFirst("1");*/
+    //  System.out.println(test.end);
+      System.out.println(test.size());
+    //  System.out.println(test.len());
+     System.out.println(test.toStringOrdered());
+    }
     @SuppressWarnings("unchecked")
     public MyDeque(){
-	data = (E[])new Object[10];
-	size = 0;
-        start = 5;
-	end = 5;
+	     data = (E[])new Object[10];
+	     size = 0;
+       start = 5;
+	     end = 5;
     }
     @SuppressWarnings("unchecked")
     public MyDeque(int initialCapacity){
-	data = (E[])new Object[initialCapacity];
-	size = 0;
-	start = initialCapacity/2;
-	end = initialCapacity/2;
+	     data = (E[])new Object[initialCapacity];
+	     size = 0;
+	     start = initialCapacity/2;
+	     end = initialCapacity/2;
     }
-    public String toString(){
-    String ans = "[";
-    if(start < end){
-      for (int i = start; i <= end; i++){
+    public int len(){
+      return data.length;
+    }
+    public String toStringOrdered(){
+      String ans = "[";
+      if(start < end){
+        for (int i = start; i <= end; i++){
+          if(data[i] != null){
         ans += data[i] + " , ";
+        }
       }
-    }
-    else{
-      for(int i = start; i < data.length; i++){
-        ans += data[i] + ", ";
-      }
-      for(int i = 0; i <= end; i++){
-        ans += data[i] + ", ";
-      }
-    }
-    ans = ans.substring(0, ans.length() - 2) + "]";
-    return ans;
-  }
-
-  public static void main(String[] args) {
-    MyDeque<String> a = new MyDeque<>(), a1 = new MyDeque<>();
-    ArrayList<String> b = new ArrayList<>();
-
-    int size = Integer.parseInt(args[0]);
-    for(int i = 0; i < size; i++){
-      int temp = (int)(Math.random() * 1000);
-      if(temp % 2 == 0){
-        a.addFirst("" + temp);
-        a1.addFirst("" + temp);
-        b.add(0, "" + temp);
       }
       else{
-        a.addLast("" + temp);
-        a1.addLast("" + temp);
-        b.add("" + temp);
+        for(int i = start; i < data.length; i++){
+            if(data[i] != null){
+          ans += data[i] + ", ";
+        }
+      }
+        for(int i = 0; i <= end; i++){
+            if(data[i] != null){
+          ans += data[i] + ", ";
+        }
+      }
+      }
+      ans = ans.substring(0, ans.length() - 2) + "]";
+      return ans;
+    }
+    public String toString(){
+      String ans = "[";
+      for(E counter : data){
+        if(counter != null){
+        ans+= counter + " ";
       }
     }
-
-    int index = 0;
-    boolean hasError = false;
-    String errorEvaluation = "Errors found at these indices: ";
-    for (String x : b){
-      if (!(x.equals(a.getFirst()))){
-        System.out.println("The getFirst() function is incorrect at index " + index);
-        hasError = true;
-      }
-      if (!(x.equals(a.removeFirst()))){
-        System.out.println("There is an error at index " + index);
-        errorEvaluation += index + ", ";
-        hasError = true;
-      }
-      index++;
-    }
-
-
-    if(hasError){
-      errorEvaluation = errorEvaluation.substring(0, errorEvaluation.length() - 2);
-      System.out.println(errorEvaluation);
-      System.out.println("MyDeque: " + a1);
-      System.out.println("Actual Deque: " + b);
-    }
-    else{
-      System.out.println("Your deque is bug-free!");
-    }
-  }
-    public int size(){
-	return size;
-    }
-    public void addFirst(E element){
-	if(element == null){
-	    throw new NullPointerException();
-	}
-	if(size == data.length ||
-	   start - 1 == end
-	   //(start == 0 && end == data.length - 1)
-     ){
-	    resize();
-	}
-  if(start == 0){
-    if(data[start] == null){
-      data[start] = element;
-    }
-    else{
-    data[data.length - 1] = element;
-    start = data.length - 1;
-  }
-}
-else{
-	data[start - 1] = element;
-	start--;
-    }
-    size+= 1;
-  }
-    public void addLast(E element){
-	if(element == null){
-	    throw new NullPointerException();
-	}
-	if(size() == data.length ||
-	   start - 1 == end 
-	   //(start == 0 && end == data.length - 1)
-     ) {
-	    resize();
-	}
-  /*if(end == data.length-1){
-    data[0] = element;
-    end = 0;
-  }*/
-	data[end + 1] = element;
-	end++;
-  size+= 1;
+      return ans + "]";
     }
     @SuppressWarnings("unchecked")
     private void resize(){
-      E[] t = (E[]) new Object[2 * data.length];
-      for(int counter = 0; counter < data.length; counter++){
-        t[counter] = data[counter];
+      E[] temp = (E[]) new Object[2 * data.length];
+      int old = start;
+      int neww = 0;
+      while(old != end){
+        temp[neww] = data[old];
+        old = (old + 1) % data.length;
+        neww++;
       }
-      data = t;
+      temp[neww] = data[end];
+      data = temp;
       start = 0;
-      end = size() - 1;
+      end = neww;
     }
-    public E getFirst(){
+    public void addFirst(E element){
+	     if(element == null){
+	        throw new NullPointerException();
+	       }
+	     if(size == data.length -1 || start - 1 == end){
+	          resize();
+	         }
+      if(start == 0){
+          if(data[data.length - 1] == null){
+            data[data.length - 1] = element;
+            start = data.length - 1;
+          }
+          else{
+            resize();
+            data[data.length - 1] = element;
+            start = data.length - 1;
+          }
+        }
+      else{
+	       data[start - 1] = element;
+	        start--;
+        }
+        size+= 1;
+      }
+    public void addLast(E element){
+	     if(element == null){
+	        throw new NullPointerException();
+	       }
+      if(size == data.length || start - 1 == end) {
+  	         resize();
+  	     }
+      if(size == 0){
+        data[end] = element;
+      }
+      else if(end == data.length - 1){
+         if(data[0] == null){
+           data[0] = element;
+           end = 0;
+         }
+         else{
+           resize();
+           data[end + 1] = element;
+           end++;
+         }
+       }
+       else{
+         data[end + 1] = element;
+	        end++;
+        }
+        size+= 1;
+    }
+    public int size(){
+      return size;
+    }
+    /*
+  public E getFirst(){
 	if(size == 0){
 	    throw new NoSuchElementException();
 	}
@@ -152,11 +159,11 @@ else{
 	return data[end];
     }
     public E removeFirst(){
-      if(size() == 0){
+      if(size == 0){
         throw new NoSuchElementException();
       }
       E answer = data[start];
-      if(start == size() - 1){
+      if(start == size - 1){
         start = 0;
       }
       else{
@@ -166,7 +173,7 @@ else{
       return answer;
       }
       public E removeLast(){
-        if(size() == 0){
+        if(size == 0){
           throw new NoSuchElementException();
         }
         E answer = data[end];
@@ -178,5 +185,5 @@ else{
         }
         size-=1;
         return answer;
-        }
+      }*/
     }
