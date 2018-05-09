1b80597e7549fefa0c10a5bb4a05642200beb422
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-05-08 22:14:44 -0400

    Worked on getNeighbors

--- 14FrontierMazeSolver/Maze.java
+++ 14FrontierMazeSolver/Maze.java
@@ -1,87 +1,180 @@
-import java.io.File;
+import java.io.*;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 public class Maze{
-  private Location start,end;
-  private char[][] board;
-  public static void main(String[] args){
-  	try{
-  	Maze f = new Maze("data3.dat");
-    System.out.println(f.toString());
-  }
-  	catch(FileNotFoundException e){
-  	    System.out.println("File not found");
-  	}
-      }
-  public Maze(String filename) throws FileNotFoundException{
-	     String mazeInfo = "";
-	     Scanner inf = new Scanner(new File(filename));
-       int rows = 0;
-       int cols = 0;
-	     while(inf.hasNextLine()){
-           String temp = inf.nextLine();;
-		       mazeInfo+= temp;
-		       rows++;
-           cols= temp.length();
-     }
-     board = new char[rows][cols];
-      int index = 0;
-	     for(int counter = 0; counter < rows; counter++){
-	       for(int counter1 = 0; counter1 < cols; counter1++){
-           board[counter][counter1] = mazeInfo.charAt(index);
-           index++;
-         }
-       }
-     }
-  // '#' - wall
-  // ' ' - open space
-  // '?' - frontier space
-  // '.' - visited space
-  // 'E' - end space (do not replace this)
-  // '@' - part of solution
-  // 'S' - starting space (do not replace this)
-  public String toString(){
-    String answer = "";
-    int c = 0;
-    for(int counter = 0; counter < board.length; counter++){
-      for(int counter1 =0; counter1 < board[counter].length; counter1++){
-        if( c == board[counter].length){
-          answer+= "\n";
-          c = 0;
-        }
-        answer+= board[counter][counter1];
-        c++;
-      }
+  private static final String CLEAR_SCREEN =  "\033[2J";
+  private static final String HIDE_CURSOR =  "\033[?25l";
+  private static final String SHOW_CURSOR =  "\033[?25h";
+  Location start,end;
+  private char[][] maze;
+
+
+
+  /*
+  YOU MUST COMPLETE THIS METHOD!!!
+  YOU MUST COMPLETE THIS METHOD!!!
+  YOU MUST COMPLETE THIS METHOD!!!
+  */
+  public Location[] getNeighbors(Location L){
+    int siz = 0;
+    int lx = L.getX();
+    int ly = L.getY();
+    Location[] answer = new Location[4];
+    int[][] arr = new int{ {0, 1}, {1, 0}, {-1, 0}, {0, -1} };
+    for(int[] i : arr){
+      if(lx + i[0] >= 0 && ly + i[1] >= 0 &&
+      lx + i[0] < maze[0].length &&
+      ly +
+      get(lx + i[0], ly + i[1]) != '#' &&
+      get(lx + i[0], ly + i[1]) != '.')
+      {
+      Location temp = new Location(lx + i[0], ly + i[1], L);
+      answer[siz] = temp;
+      siz++;
     }
     return answer;
   }
-
-  // Work on this method as a group!
-  /*public String toStringColor(){
-  }*/
-
-  //return a list of Locations that are:
-  // adjacent to n and  not visited
-  // all the Locations in this list should have their previous set to n.
-  public Location[] getNeighbors(Location n){
-    /*int tx = n.getX();
-    int ty = n.getY();
-    Location[] answer;
-    if(tx - 1 >= 0 && board[tx - 1][ty] != '#' && board[tx - 1][ty] != '.' ){
-
-    }*/
-    Location[] tempAnswer = new Location[4];
-    return tempAnswer;
-  }
+}
 
   public Location getStart(){
     return start;
   }
-
   public Location getEnd(){
     return end;
   }
 
 
+  private static String go(int x,int y){
+    return ("\033[" + x + ";" + y + "H");
+  }
+  private static String color(int foreground,int background){
+    return ("\033[0;" + foreground + ";" + background + "m");
+  }
 
+  public void clearTerminal(){
+    System.out.println(CLEAR_SCREEN+"\033[1;1H");
+  }
+  public Maze(String filename){
+    ArrayList<char[]> lines = new ArrayList<char[]>();
+    int startr=-1, startc=-1;
+    int endr=-1,endc=-1;
+    try{
+      Scanner in = new Scanner(new File(filename));
+      while(in.hasNext()){
+        lines.add(in.nextLine().toCharArray());
+      }
+    }catch(FileNotFoundException e){
+      System.out.println("File not found: "+filename);
+      System.exit(1);
+    }
+    maze = new char[lines.size()][];
+    for(int i = 0; i < maze.length; i++){
+      maze[i]=lines.get(i);
+    }
+    for(int r=0; r<maze.length;r++){
+      for(int c=0; c<maze[r].length;c++){
+        if(maze[r][c]=='S'){
+          if(startr == -1){
+            startr=r;
+            startc=c;
+          }else{
+            System.out.println("Multiple 'S' found!");
+            System.exit(0);
+          }
+        }
+
+        if(maze[r][c]=='E'){
+          //erase E
+          //maze[r][c]=' ';
+          if(endr == -1){
+            endr=r;
+            endc=c;
+          }else{
+            System.out.println("Multiple 'E' found!");
+            System.exit(0);
+          }
+        }
+      }
+    }
+    if(startr == -1 || endr == -1){
+      System.out.println("Missing 'S' or 'E' from maze.");
+      System.exit(0);
+
+    }
+
+    /*
+    THIS IS AN IMPORTANT PART BECAUSE YOU WILL NEED TO CHANGE IT LATER!
+    The start/end Locations may need more information later when we add
+    other kinds of frontiers!
+    */
+    end = new Location(endr,endc,null);
+    start = new Location(startr,startc,null);
+  }
+
+  public String toStringColor(){
+    return toStringColor(50);
+  }
+
+  public String toStringColor(int delay){
+    try{
+      Thread.sleep(delay);
+    }catch(InterruptedException e){
+
+    }
+    return HIDE_CURSOR+CLEAR_SCREEN+go(1,1)+colorize(toString())+SHOW_CURSOR;
+  }
+
+  public String toString(){
+    int maxr = maze.length;
+    int maxc = maze[0].length;
+    String ans = "";
+    for(int i = 0; i < maxr * maxc; i++){
+      int row = i/maxc;
+      int col = i%maxc;
+
+      char c =  maze[row][col];
+      ans+=c;
+      if( col == maxc-1 ){
+        ans += "\n";
+      }
+
+    }
+    return ans + "\n";
+  }
+
+  public char get(int row,int col){
+    return maze[row][col];
+  }
+  public void set(int row,int col, char n){
+    maze[row][col] = n;
+  }
+  public static String colorize(String s){
+    String ans = "";
+    Scanner in = new Scanner(s);
+    while(in.hasNext()){
+      String line ="";
+      for(char c : in.nextLine().toCharArray()){
+        if(c == '#'){
+          line+= color(37,47)+c;
+        }
+        else if(c == '@'){
+          line+= color(33,40)+c;
+        }
+        else if(c == '?'){
+          line+= color(37,42)+c;
+        }
+        else if(c == '.'){
+          line+= color(36,40)+c;
+        }
+        else if(c == ' '){
+          line+= color(35,40)+c;
+        }else{
+          line+=color(37,40)+c;
+        }
+
+      }
+      ans += line+color(37,40)+"\n";
+    }
+    return ans;
+  }
 }
