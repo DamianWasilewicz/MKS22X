1100cd37e41e2b80185e063b838e76187a95933f
DamianWasilewicz <dwasilewicz@stuy.edu>
2018-05-14 15:36:01 -0400

    started Astar

--- 14FrontierMazeSolver/Location.java
+++ 14FrontierMazeSolver/Location.java
@@ -1,31 +1,35 @@
 public class Location implements Comparable<Location>{
-  private int x, y, priority;
-  private Location previous;
-  public Location(int _x, int _y, Location prev, int dist){
-    x = _x;
-    y = _y;
-    previous = prev;
-    priority = dist;
-  }
-  public int getX(){
-    return x;
-  }
-  public int getY(){
-    return y;
-  }
-  public Location getPrev(){
-    return previous;
-  }
-  public String toString(){
-    return x + " " +y;
-  }
-  public boolean equals(Location o){
-    return x == o.getX() && y == o.getY();
-  }
-  public int compareTo(Location o){
-    return getPriority() - o.getPriority();
-  }
-  public int getPriority(){
-    return priority;
-  }
+    private int x, y, priority, dis;
+    private Location previous;
+    public Location(int _x, int _y, Location prev, int dist, int prevD){
+	x = _x;
+	y = _y;
+	previous = prev;
+	priority = dist;
+	dis = prevD;
+    }
+    public int getX(){
+	return x;
+    }
+    public int getY(){
+	return y;
+    }
+    public Location getPrev(){
+	return previous;
+    }
+    public String toString(){
+	return x + " " +y;
+    }
+    public boolean equals(Location o){
+	return x == o.getX() && y == o.getY();
+    }
+    public int compareTo(Location o){
+	return getPriority() - o.getPriority();
+    }
+    public int getPriority(){
+	return priority;
+    }
+    public int getDist(){
+	return dis;
+    }
 }
